9f6b7cfacfa5e624c655a20ac654645cc47cdf19
DeveloperHIT <akajan@myintellego.com>
2023-09-15 12:56:20 -0400

    Add responsiveness

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,43 +1,20 @@
 "use client";
 import Hero from "@/sections/Hero";
-import { alpha, Box } from "@mui/material";
-import TrendingInsights from "@/sections/TrendingInsights";
-import Services from "@/sections/Services";
-import Industry from "@/sections/Industry";
-import Insights from "@/sections/Insights";
-import Footer from "@/components/Footer/Footer";
-import theme from "@/components/ThemeRegistry/theme";
-import Container from "@/components/Container";
+import { Box } from "@mui/material";
 
 export default function Home() {
   return (
     <Box>
-      <Container
+      <Box
         sx={{
           backgroundImage: `url("hero.png")`,
           position: "relative",
-          "&::after": {
-            position: "absolute",
-            content: '""',
-            width: "20%",
-            zIndex: 1,
-            top: 0,
-            left: 0,
-            height: "100%",
-            opacity: 0.2,
-          },
         }}
       >
         <Box position={"relative"} zIndex={2}>
           <Hero />
         </Box>
-      </Container>
-      {/*<Hero />*/}
-      {/*<TrendingInsights />*/}
-      {/*<Services />*/}
-      {/*<Industry />*/}
-      {/*<Insights />*/}
-      {/*<Footer />*/}
+      </Box>
     </Box>
   );
 }
--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,38 +1,18 @@
 import React from "react";
 import { Box, Grid, Typography } from "@mui/material";
+import Container from "@/components/Container";
 
 // TODO: Fade the bottom of the hero image
 
 const HeroSection: React.FC = () => {
   return (
-    // <Box
-    //   sx={{
-    //     height: "700px",
-    //     width: "100vw",
-    //     backgroundImage: `url(hero.png)`,
-    //     backgroundSize: "cover",
-    //     backgroundPosition: "center",
-    //     backgroundRepeat: "no-repeat",
-    //     display: "flex",
-    //     justifyContent: "center",
-    //     alignItems: "center",
-    //     paddingTop: "100px",
-    //     paddingBottom: "100px",
-    //   }}
-    // >
-    //   <Box display="flex" flexDirection="column" sx={{ paddingLeft: 2 }}>
-    //     <Typography variant="h1" sx={{ color: "white", fontWeight: 500 }}>
-    //       Industry leader in
-    //       <br /> healthcare fair market value
-    //     </Typography>
-    //     <Typography variant="h5" sx={{ color: "white", fontWeight: 300 }}>
-    //       Navigating the complexities of healthcare compliance with expertise
-    //       and efficiency
-    //     </Typography>
-    //   </Box>
-    // </Box>
-    <Box sx={{ paddingTop: 10 }}>
-      <Box marginBottom={4}>
+    <Container>
+      <Box
+        marginBottom={4}
+        sx={{
+          paddingTop: 10,
+        }}
+      >
         <Box marginBottom={2}>
           <Typography
             variant="h3"
@@ -66,7 +46,7 @@ const HeroSection: React.FC = () => {
         </Box>
         <Grid container spacing={2}></Grid>
       </Box>
-    </Box>
+    </Container>
   );
 };
 

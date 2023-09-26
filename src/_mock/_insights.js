8719f2367a6bd3325f1c57e8258e6e0ee51cd720
DeveloperHIT <akajan@myintellego.com>
2023-09-25 21:29:47 -0400

    Add better mocks

--- src/_mock/_insights.js
+++ src/_mock/_insights.js
@@ -6,26 +6,60 @@ export const __insights = [
     createdAt: "2021-09-30T00:00:00.000Z",
     content: "insight",
     coverUrl: _mock.image.insights,
-    description: "Description",
+    description:
+      "An in-depth look into Stark Law, its objectives, and its implications for healthcare providers.",
     tags: ["tag1", "tag2"],
-    title: "Title",
+    title: "Understanding Stark Law: A Comprehensive Guide",
   },
   {
     author: "Author",
     createdAt: "2021-09-30T00:00:00.000Z",
     content: "insight",
     coverUrl: _mock.image.insights,
-    description: "Description",
+    description:
+      "Unveiling the methodologies and best practices in determining the fair market value within the healthcare sector.",
     tags: ["tag1", "tag2"],
-    title: "Title",
+    title: "Determining Fair Market Value in Healthcare: Best Practices",
   },
   {
     author: "Author",
     createdAt: "2021-09-30T00:00:00.000Z",
     content: "insight",
     coverUrl: _mock.image.insights,
-    description: "Description",
+    description:
+      "Practical strategies for healthcare institutions to avoid violations and maintain compliance with Anti-Kickback regulations.",
     tags: ["tag1", "tag2"],
-    title: "Title",
+    title:
+      "Avoiding Anti-Kickback Violations: Key Strategies for Healthcare Providers",
+  },
+  {
+    author: "Author",
+    createdAt: "2021-09-30T00:00:00.000Z",
+    content: "insight",
+    coverUrl: _mock.image.insights,
+    description:
+      "Exploring the intricate relationship between asset valuation in healthcare and the mandates set by Stark Law.",
+    tags: ["tag1", "tag2"],
+    title: "The Intersection of Stark Law and Fair Market Valuation",
+  },
+  {
+    author: "Author",
+    createdAt: "2021-09-30T00:00:00.000Z",
+    content: "insight",
+    coverUrl: _mock.image.insights,
+    description:
+      "Debunking prevalent misconceptions surrounding Anti-Kickback regulations and shedding light on actual stipulations.",
+    tags: ["tag1", "tag2"],
+    title: "5 Common Misconceptions about Anti-Kickback Regulations",
+  },
+  {
+    author: "Author",
+    createdAt: "2021-09-30T00:00:00.000Z",
+    content: "insight",
+    coverUrl: _mock.image.insights,
+    description:
+      "Debunking prevalent misconceptions surrounding Anti-Kickback regulations and shedding light on actual stipulations.",
+    tags: ["tag1", "tag2"],
+    title: "Challenges in Healthcare Asset Valuation: Expert Insights",
   },
 ];

9fbc9b3c706f577d7aa485aa88bd8fd0b82ee9c5
DeveloperHIT <akajan@myintellego.com>
2023-11-07 11:03:39 -0500

    Delete types bucket file

--- src/app/(user)/page.tsx
+++ src/app/(user)/page.tsx
@@ -2,6 +2,7 @@ import LandingView from "@/sections/view/landingView";
 import { getInsights } from "@/sanity/sanity-utils";
 
 export default async function Home() {
+  // TODO: Replace this with new queries
   const insights = await getInsights();
 
   return <LandingView insights={insights} />;
--- src/types/index.ts
+++
@@ -1,5 +0,0 @@
-import { Author } from "@/types/Author";
-import { Insight } from "@/types/Insight";
-import { Project } from "@/types/Project";
-
-export type { Author, Insight, Project };

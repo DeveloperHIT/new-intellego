be0316e25fd7b3b2f3902d6a44ec7a90e6f478e1
DeveloperHIT <akajan@myintellego.com>
2023-11-10 12:06:25 -0500

    Debug typescript

--- src/sanity/studio/structure/defaultDocumentNodeResolver.ts
+++ src/sanity/studio/structure/defaultDocumentNodeResolver.ts
@@ -1,12 +1,16 @@
 import JsonPreview from "@/sanity/components/JsonPreview";
 
+interface Props {
+  S: any;
+}
+
 /**
  * Returns the default view for a document node.
  *
  * @param {Object} S - The structure builder object.
  * @returns {Object} The default document node with predefined views.
  */
-export const defaultDocumentNodeResolver = (S) =>
+export const defaultDocumentNodeResolver = ({ S }: Props) =>
   S.document().views([
     S.view.form(),
     S.view.component(JsonPreview).title("JSON"),

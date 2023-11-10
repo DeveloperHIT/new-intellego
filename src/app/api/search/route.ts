bb04b42a45c95f6da07a7bdc282c2c7a4a9f9258
DeveloperHIT <akajan@myintellego.com>
2023-11-10 15:14:48 -0500

    Algolia post request

--- src/app/api/search/route.ts
+++ src/app/api/search/route.ts
@@ -1,7 +1,7 @@
 import algoliasearch from "algoliasearch";
 import indexer from "sanity-algolia";
 import { createClient } from "next-sanity";
-
+import { NextRequest, NextResponse } from "next/server";
 import { apiVersion, dataset, projectId, useCdn } from "@/sanity/env";
 
 // Create algolia client
@@ -9,8 +9,6 @@ const algolia = algoliasearch(
   "A75DQ286VR",
   process.env.ALGOLIA_ADMIN_KEY as string,
 );
-
-console.log("algolia ", algolia);
 
 // Create sanity client
 export const sanity = createClient({
@@ -20,25 +18,39 @@ export const sanity = createClient({
   useCdn,
 });
 
-console.log("sanity ", sanity);
+// Function to handle POST requests
+export async function POST(request: NextRequest) {
+  try {
+    // Collect the body as a string and parse it as JSON
+    const body = await request.json();
 
-export async function POST(request: Request) {
-  const sanityAlgolia = indexer(
-    {
-      post: {
-        index: algolia.initIndex("live_posts"),
+    const sanityAlgolia = indexer(
+      {
+        post: {
+          index: algolia.initIndex("live_posts"),
+        },
       },
-    },
-    (document) => {
-      console.log("Document ", document);
-      switch (document._type) {
-        case "post":
-          return {
-            title: document.title,
-          };
-        default:
-          throw new Error(`Unknown type: ${document._type}`);
-      }
-    },
-  );
+      (document) => {
+        // Document transformation logic
+        switch (document._type) {
+          case "post":
+            return {
+              title: document.title,
+            };
+          default:
+            throw new Error(`Unknown type: ${document._type}`);
+        }
+      },
+    );
+
+    // Perform the webhook sync with the parsed body
+    await sanityAlgolia.webhookSync(sanity, body);
+
+    // Return a success response
+    return new NextResponse("success", { status: 200 });
+  } catch (error) {
+    // If there's an error, log it and return an error response
+    console.error("Error processing request:", error);
+    return new NextResponse("Error processing request", { status: 500 });
+  }
 }

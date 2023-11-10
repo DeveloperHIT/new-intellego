965e52383f4d9f88869269fa7165cddb46028014
DeveloperHIT <akajan@myintellego.com>
2023-11-10 18:49:29 -0500

    Amend environment variables

--- src/app/api/search/route.ts
+++ src/app/api/search/route.ts
@@ -1,15 +1,12 @@
 import algoliasearch from "algoliasearch";
-// import indexer from "sanity-algolia";
+import indexer from "sanity-algolia";
 import { createClient } from "next-sanity";
-import {
-  // NextRequest,
-  NextResponse,
-} from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { apiVersion, dataset, projectId, useCdn } from "@/sanity/env";
 
 // Create algolia client
 const algolia = algoliasearch(
-  process.env.ALGOLIA_APPLICATION_ID as string,
+  process.env.NEXT_PUBLIC_ALGOLIA_APPLICATION_ID as string,
   process.env.ALGOLIA_ADMIN_API_KEY as string,
 );
 
@@ -39,6 +36,22 @@ const query = `*[
   "author": author -> {_id, name}
 }`;
 
+const post_projection = `{
+    _type, 
+    _rev, 
+    "objectID": _id, 
+    _createdAt,
+    
+    title,
+    "slug": slug.current,
+    "categories": categories[] -> {
+        _id, title
+    },
+    "tags": tags[] -> {_id, title},
+    "author": author -> {_id, name}
+    }`;
+
+// GET request is confirmed to work
 export async function GET() {
   // Fetch all posts based on query
   const posts = await sanity.fetch(query);
@@ -51,6 +64,34 @@ export async function GET() {
     // Save objects to the index
     await index.saveObjects(posts);
     // Return a success response
+    return new NextResponse("success", { status: 200 });
+  } catch (error: any) {
+    console.error("Error details:", error);
+    return new NextResponse(`Error processing request: ${error.message}`, {
+      status: 500,
+    });
+  }
+}
+
+// POST request is confirmed to work
+export async function POST(request: NextRequest) {
+  try {
+    const body = await request.json();
+
+    const index = algolia.initIndex(process.env.ALGOLIA_INDEX as string);
+
+    const sanityAlgolia = indexer(
+      {
+        post: {
+          index,
+          projection: post_projection,
+        },
+      },
+      (document) => document,
+      // TODO: If we want an approval process, add logic only to add if status is approved here
+    );
+
+    await sanityAlgolia.webhookSync(sanity, body);
     return new NextResponse("success", { status: 200 });
   } catch (error: any) {
     console.error("Error details:", error);
